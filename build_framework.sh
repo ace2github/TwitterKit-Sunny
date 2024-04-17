ea9ce5594bdf021c5210c53bca6b38d94c5abdf5
Lockie <173141667@qq.com>
2024-04-17 08:42:21 +0800

    only ios

--- build_framework.sh
+++ build_framework.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-## Build TwitterKit.framework - x86_64
-xcodebuild \
-    -project TwitterKit/TwitterKit.xcodeproj \
-    -scheme TwitterKit -configuration Debug \
-    -sdk "iphonesimulator" \
-    HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphonesimulator/Headers $(pwd)/TwitterCore/iphonesimulator/PrivateHeaders"  \
-    CONFIGURATION_BUILD_DIR=./iphonesimulator \
-    clean build
+# ## Build TwitterKit.framework - x86_64
+# xcodebuild \
+#     -project TwitterKit/TwitterKit.xcodeproj \
+#     -scheme TwitterKit -configuration Release \
+#     -sdk "iphonesimulator" \
+#     CFBundleSupportedPlatforms=iPhoneSimulator
+#     HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphonesimulator/Headers $(pwd)/TwitterCore/iphonesimulator/PrivateHeaders"  \
+#     CONFIGURATION_BUILD_DIR=./iphonesimulator \
+#     clean build
 
 ## Build TwitterKit.framework - armv7, arm64
 xcodebuild \
     -project TwitterKit/TwitterKit.xcodeproj \
-    -scheme TwitterKit -configuration Debug \
+    -scheme TwitterKit -configuration Release \
     -sdk "iphoneos" \
+    CFBundleSupportedPlatforms=iphoneos \
     HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphoneos/Headers $(pwd)/TwitterCore/iphoneos/PrivateHeaders"  \
     CONFIGURATION_BUILD_DIR=./iphoneos \
     clean build
@@ -24,8 +26,8 @@ xcodebuild \
 rm -rf iOS
 mkdir -p iOS
 cp -r TwitterKit/iphoneos/TwitterKit.framework/ iOS/TwitterKit.framework
-lipo -create -output iOS/TwitterKit.framework/TwitterKit TwitterKit/iphoneos/TwitterKit.framework/TwitterKit TwitterKit/iphonesimulator/TwitterKit.framework/TwitterKit
-lipo -archs iOS/TwitterKit.framework/TwitterKit
+# lipo -create -output iOS/TwitterKit.framework/TwitterKit TwitterKit/iphoneos/TwitterKit.framework/TwitterKit TwitterKit/iphonesimulator/TwitterKit.framework/TwitterKit
+# lipo -archs iOS/TwitterKit.framework/TwitterKit
 
 ## Zip them into TwitterKit.zip
 rm TwitterKit.zip
